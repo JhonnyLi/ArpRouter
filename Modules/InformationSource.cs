1679c974dd84b52ac53b32e4ca9438b376ebc5cf
Jhonny Li <jhonny@jhonny.se>
2019-10-07 01:10:57 +0200

    Added initial SharpPcap code. #1 Added new Test project #6 Added xUnit and some initial SharpPcap tests

---
+++ Modules/InformationSource.cs
@@ -0,0 +1,45 @@
+﻿using ArpRouter.Modules;
+using PacketDotNet;
+using SharpPcap;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArpRouter.Modules
+{
+    public class InformationSource : NetworkInformation
+    {
+        private readonly ICaptureDevice captureDevice;
+
+        public event EventHandler NetworkAlert;
+
+        public InformationSource(TestCaptureDevice device)
+        {
+            captureDevice = device;
+            captureDevice.OnPacketArrival += CaptureDevice_OnPacketArrival;
+        }
+
+        private void CaptureDevice_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            AddNetworkMessage(e.Packet.Data.ToString());
+            NetworkEventArgs netArgs = ExtractNetworkInformation(e.Packet);
+
+            NetworkAlert(this, netArgs);
+        }
+
+        private NetworkEventArgs ExtractNetworkInformation(RawCapture packet)
+        {
+            EthernetPacket ethernetPacket = PacketDotNet.Packet.ParsePacket(LinkLayers.Ethernet, packet.Data) as EthernetPacket;
+            IPPacket ipPacket = ethernetPacket.Extract<IPPacket>();
+            TcpPacket tcpPacket = ethernetPacket.Extract<TcpPacket>();
+
+            return new NetworkEventArgs(ipPacket.DestinationAddress.ToString(), tcpPacket.DestinationPort.ToString(), ipPacket.SourceAddress.ToString(), tcpPacket.SourcePort.ToString());
+        }
+
+        public void StartListening()
+        {
+            captureDevice.StartCapture();
+        }
+
+    }
+}
